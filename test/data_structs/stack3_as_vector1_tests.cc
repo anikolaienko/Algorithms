7d97b08e9a0b9eae440fb31fe49aa1d76715cca8
Andrew Nikolayenko <nikolayenko.andrew@gmail.com>
2020-06-20 19:00:14 +0400

    added more tests

--- test/data_structs/stack3_as_vector1_tests.cc
+++ test/data_structs/stack3_as_vector1_tests.cc
@@ -52,3 +52,33 @@ TEST(Stack3AsVector1, ResizeStack3) {
         expected.pop_back();
     }
 }
+
+TEST(Stack3AsVector1, ResizeStack123) {
+    Algorithms::Stack3 stack3;
+    stack3.push1(10);
+    stack3.push1(11);
+    stack3.push1(12);
+    EXPECT_EQ(stack3.size1(), 3);
+    
+    stack3.push2(20);
+    stack3.push2(21);
+    stack3.push2(22);
+    EXPECT_EQ(stack3.size2(), 3);
+
+    stack3.push3(30);
+    stack3.push3(31);
+    stack3.push3(32);
+    EXPECT_EQ(stack3.size3(), 3);
+
+    stack3.push1(13);
+    stack3.push1(14);
+    EXPECT_EQ(stack3.size1(), 5);
+
+    stack3.push2(23);
+    stack3.push2(24);
+    EXPECT_EQ(stack3.size2(), 5);
+
+    for (int x: {32, 31, 30}) EXPECT_EQ(stack3.pop3(), x);
+    for (int x: {24, 23, 22, 21, 20}) EXPECT_EQ(stack3.pop2(), x);
+    for (int x: {14, 13, 12, 11, 10}) EXPECT_EQ(stack3.pop1(), x);
+}
